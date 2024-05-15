e547cd60c476d2ce4855946e24f0e225cac43089
Gosqu248 <grzegorzurban248@gmail.com>
2024-05-15 18:15:22 +0200

    Game added

---
+++ Assets/Scripts/DoorManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+        bool doorIsOpen = false;
+        float doorTimer = 0.0f;
+        public float doorOpenTime = 3.0f;
+        public AudioClip doorOpenSound;
+        public AudioClip doorShutSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if(doorIsOpen) {
+            doorTimer += Time.deltaTime;
+        }
+
+        if(doorTimer > doorOpenTime) {
+            Door(false, doorShutSound, "DoorShut");
+            doorTimer = 0.0f;
+        }
+    }
+
+    void DoorCheck() {
+        if(!doorIsOpen) {
+            Door(true, doorOpenSound, "DoorOpen");
+        }
+    }
+
+    void Door(bool openCheck, AudioClip aClip, string animName) {
+            doorIsOpen = openCheck;
+            this.GetComponent<AudioSource>().PlayOneShot(aClip);
+            this.transform.parent.GetComponent<Animation>().Play(animName);
+    }
+}
